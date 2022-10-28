207d670211dd418fbb552d3088d193676565375d
anabneri <ananeri32@hotmail.com>
2022-10-28 20:18:58 -0300

    minor

--- src/chain/ATMBills/before/ATM.java
+++ src/chain/ATMBills/before/ATM.java
@@ -8,17 +8,17 @@ public class ATM {
 		if(remaining >= 50) {
 			int bills = remaining / 50;
 			remaining %= 50;
-			System.out.println(String.format("- %d bill(s) of $%d, $%d remaining", bills, 50, remaining));
+			System.out.printf("- %d bill(s) of $%d, $%d remaining%n", bills, 50, remaining);
 		}
 		if(remaining >= 10) {
 			int bills = remaining / 10;
 			remaining %= 10;
-			System.out.println(String.format("- %d bill(s) of $%d, $%d remaining", bills, 10, remaining));
+			System.out.printf("- %d bill(s) of $%d, $%d remaining%n", bills, 10, remaining);
 		}
 		if(remaining >= 1) {
-			int bills = remaining / 1;
+			int bills = remaining;
 			remaining %= 1;
-			System.out.println(String.format("- %d bill(s) of $%d, $%d remaining", bills, 1, remaining));
+			System.out.printf("- %d bill(s) of $%d, $%d remaining%n", bills, 1, remaining);
 		}
 	}
 }
